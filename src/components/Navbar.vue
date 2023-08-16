9aac9822b50e52a2f3596b8c935dbb4176c94443
rifki alamsyah <m.rifki_si19@nusaputra.ac.id>
2023-08-16 10:01:01 +0700

    Starter Navber

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,94 @@
+<template>
+  <nav class="bg-white shadow-lg">
+    <div class="max-w-6xl mx-auto px-4">
+      <div class="flex justify-between">
+        <div class="flex space-x-7">
+          <div>
+            <a href="#" class="flex items-center py-4 px-2">
+              <img src="../assets/logo.svg" alt="Logo" class="h-8 w-8 mr-2">
+              <span class="font-semibold text-gray-500 text-lg">Navigation</span>
+            </a>
+          </div>
+          <div class="hidden md:flex items-center space-x-1">
+            <a href="" class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold">Home</a>
+            <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Services</a>
+            <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">About</a>
+            <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Contact Us</a>
+          </div>
+        </div>
+        <div class="hidden md:flex items-center space-x-3 ">
+          <a href="" class="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300">Log In</a>
+          <a href="" class="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Sign Up</a>
+        </div>
+        <div class="md:hidden flex items-center">
+          <button class="outline-none mobile-menu-button" @click="toggleMenu">
+            <svg class="w-6 h-6 text-gray-500 hover:text-green-500" :class="{ 'text-green-500': showMenu }"
+                 fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                 stroke="currentColor">
+              <path d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div :class="{ 'hidden': !showMenu }" class="mobile-menu">
+      <ul class="">
+        <li><a href="index.html" class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
+        <li><a href="#services" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
+        <li><a href="#about" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a></li>
+        <li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
+
+
+const showMenu = ref(false);
+
+const toggleMenu = () => {
+  showMenu.value = !showMenu.value;
+};
+</script>
+
+<style scoped>
+.fade-in-down {
+  opacity: 0;
+  transform: translateY(-50px);
+  animation: fade-in-down .5s ease forwards;
+}
+
+@keyframes fade-in-down {
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@media (min-width: 1000px) { 
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.navbar-brand {
+  margin-bottom: 10px;
+}
+
+.navbar-nav {
+  display: flex;
+  justify-content: center;
+}
+}
+
+
+</style>
+
+
+<!-- 
 
 // Menyimpan posisi scroll sebelumnya
 let prevScrollPos = window.pageYOffset;
@@ -37,67 +126,6 @@ onMounted(() => {
 onUnmounted(() => {
   window.removeEventListener('scroll', handleScroll);
 });
-</script>
-<template lang="">
-  <div>
-    <nav class="navbar navbar-expand-lg bg-dark  navbar-dark">
-      <div class="container">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand fs-3" href="#">
-           Megumi Exclusive</a>  
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <router-link to="/" class="nav-link"  :class="{ active: $route.path === '/' }"><h5>Home</h5></router-link>
-            <router-link to ="/movie" class="nav-link"  :class="{ active: $route.path === '/movie' }"><h5>Movies</h5></router-link>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </div>
-</template>
-
-<style scoped>
-.fade-in-down {
-  opacity: 0;
-  transform: translateY(-50px);
-  animation: fade-in-down .5s ease forwards;
-}
-
-@keyframes fade-in-down {
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
-
-@media (min-width: 1000px) { 
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.navbar-brand {
-  margin-bottom: 10px;
-}
-
-.navbar-nav {
-  display: flex;
-  justify-content: center;
-}
-}
 
 
-
-
-</style>
+ -->
